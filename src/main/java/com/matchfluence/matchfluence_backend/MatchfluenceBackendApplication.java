300e1678d04c2c59e9a05802f870716d8f7be38e
wiktor <nobis171@wp.pl>
2026-06-02 16:55:27 +0200

    Initial commit

---
+++ src/main/java/com/matchfluence/matchfluence_backend/MatchfluenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matchfluence.matchfluence_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchfluenceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchfluenceBackendApplication.class, args);
+	}
+
+}
